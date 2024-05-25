d277fe9d822c52a293d9700dc0089912dd0854be
Matthew J. Mucklo <mmucklo@gmail.com>
2024-05-25 09:38:01 -0700

    fix string literal format issues

--- logging/auto_roll_logger.cc
+++ logging/auto_roll_logger.cc
@@ -118,7 +118,7 @@ class AutoRollLoggerTest : public testing::Test {
   }
 
   static const std::string kSampleMessage;
-  static const std::string kSampleMessageFmt;
+  static const char* const kSampleMessageFmt;
   static const std::string kTestDir;
   static const std::string kTestDbDir;
   static const std::string kLogFile;
@@ -127,8 +127,8 @@ class AutoRollLoggerTest : public testing::Test {
 
 const std::string AutoRollLoggerTest::kSampleMessage(
     "this is the message to be written to the log file!!");
-const std::string AutoRollLoggerTest::kSampleMessageFmt(
-    kSampleMessage + ":LogHeaderTest line %d");
+const char* const AutoRollLoggerTest::kSampleMessageFmt(
+    "this is the message to be written to the log file!!:LogHeaderTest line %d");
 const std::string AutoRollLoggerTest::kTestDir(
     test::PerThreadDBPath("db_log_test"));
 const std::string AutoRollLoggerTest::kTestDbDir(
@@ -627,7 +627,7 @@ TEST_F(AutoRollLoggerTest, LogHeaderTest) {
     int i = 0;
     for (size_t iter = 0; iter < 2; iter++) {
       while (logger.GetLogFileSize() < LOG_MAX_SIZE) {
-        Info(&logger, kSampleMessageFmt.c_str(), i);
+        Info(&logger, kSampleMessageFmt, i);
         ++i;
       }
 
